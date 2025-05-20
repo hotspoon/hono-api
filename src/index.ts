c375d8d3499263de5a6d56c892718320f968a7a9
Al Farisi <fariez18@outlook.com>
2025-05-20 13:48:06 +0700

    chore: update dependencies and configure Vercel
    - Added "vercel" as a dev dependency in package.json.
    - Removed the import of handle from "hono/vercel" and exported the app directly in index.ts.
    - Created a new vercel.json file to set up rewrites for routing.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono"
-import { handle } from "hono/vercel"
 import { logger } from "hono/logger"
 import { errorHandler } from "./middlewares/errorHandler"
 import { notFoundHandler } from "./middlewares/notFoundHandler"
@@ -17,4 +16,4 @@ app.onError(errorHandler)
 app.use(logger())
 app.route("/", router)
 
-export default handle(app)
+export default app
