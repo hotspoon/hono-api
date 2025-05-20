c9f9dbafeda185316428c18ebabf0b751cd0faac
Al Farisi <fariez18@outlook.com>
2025-05-20 11:40:52 +0700

    refactor: remove unused devDependency and update export for Vercel compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono"
+import { handle } from "hono/vercel"
 import { logger } from "hono/logger"
 import { errorHandler } from "./middlewares/errorHandler"
 import { notFoundHandler } from "./middlewares/notFoundHandler"
@@ -16,4 +17,4 @@ app.onError(errorHandler)
 app.use(logger())
 app.route("/", router)
 
-export default app
+export default handle(app)
