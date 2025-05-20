586c8fb929127c11c78c9613ddc03fe5059e4887
Al Farisi <fariez18@outlook.com>
2025-05-20 15:34:29 +0700

    refactor: update handler exports for HTTP methods in index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,4 +29,10 @@ app.get("/", (c) =>
   })
 )
 
-export default handle(app)
+const handler = handle(app)
+
+export const GET = handler
+export const POST = handler
+export const PATCH = handler
+export const PUT = handler
+export const OPTIONS = handler
